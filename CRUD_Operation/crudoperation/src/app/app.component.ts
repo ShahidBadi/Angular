fa0d8a8a66ba93bc966acc8ebd546d63db056fc6
ShahidBadi <24010101606@darshan.ac.in>
2025-01-02 13:31:05 +0530

    lab-10

--- CRUD_Operation/crudoperation/src/app/app.component.ts
+++ CRUD_Operation/crudoperation/src/app/app.component.ts
@@ -15,28 +15,46 @@ export class AppComponent {
   button="insert"
   editid:any=-1
   arr=["shahid","badi","wankaner"]
+  // secarr=[...this.arr]
   insert(){
-    if(this.value!="")
+    if(this.value!="" && this.editid<0)
     {
-      if(this.editid<0){
         this.arr.push(this.value);
-      }
-      else{
-        this.arr[this.editid] = this.value;
+        
+      // this.arr=[...this.secarr]
+    }
+    this.arr[this.editid] = this.value;
         this.editid = -1;
         this.button="insert"
-      }
-    }
     
 
     this.value=""
   }
   delete(id:any){
     this.arr.splice(id,1)
+    // this.arr=[...this.secarr]
   }
   update(id:number){
+    let bool=confirm("confirm to update")
+    if(bool){
       this.value=this.arr[id]
       this.editid=id;
       this.button="update"
+    }
   }
+  searchval:any=""
+  searchname:any=""
+  index:any=""
+  // search(){
+  //    if(this.searchval="")
+  //    {
+  //     this.arr=[...this.secarr]
+  //    }
+  //    this.arr=this.arr.filter((elem)=>{elem.toLowerCase().includes(this.searchval.toLowerCase())})
+  // }
+  search() {
+    return this.arr.filter((item: string) =>
+      item.toLowerCase().includes(this.searchval.toLowerCase())
+    );
+}
 }
